915a71fb77e2f4ce427d6b40155583dea2d24eaa
Qwerty1571 <86067649+ali1571@users.noreply.github.com>
2026-02-27 10:22:01 +0500

    Update settings.py

--- solsite/settings.py
+++ solsite/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'mnemonic-gen.vercel.app').split(',')
 
 
 # Application definition
